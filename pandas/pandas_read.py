d6566efc5e93cdd5347b59c5f719ac9b18af4da7
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-06-01 10:43:09 +0900

    Update pandas_read.py

--- pandas/pandas_read.py
+++ pandas/pandas_read.py
@@ -1,3 +1,11 @@
 import pandas as pd
+
+######## first row : columns
 df = pd.read_csv('123.csv')
+df
+
+
+######## specifiy column names
+colnames=['background', "Gallbladder", "Liver", "Pancreas", "Spleen", "Stomach"]
+df = pd.read_csv('test.csv',names=colnames, header=None)
 df
