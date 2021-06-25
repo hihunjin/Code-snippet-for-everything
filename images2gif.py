8a64e27c7ee61d37be9bc21ed0ce648dfb61ddcc
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-06-25 17:43:12 +0900

    Update images2gif.py

--- images2gif.py
+++ images2gif.py
@@ -4,7 +4,7 @@ import numpy as np
 # collect images
 images = []
 for _ in range(10):
-    images.append(np.uint8(np.random.randint(0,2,(512,512))))
+    images.append(np.uint8(np.random.randint(0,255,(512,512))))
 
 # to gif
-imageio.mimsave('movie.gif', images)
+imageio.mimsave('movie.gif', images, format='GIF', duration=0.2) # duration unit = second | format='GIF' 는 없어도 됨.
