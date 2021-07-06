efff71436937b840df3cd12f49047a52475cf8fc
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-07-06 12:42:22 +0900

    Create load_save_nifty_nib.py

---
+++ load_save_nifty_nib.py
@@ -0,0 +1,11 @@
+# load and save the same image with nibabel
+import nibabel as nib
+
+# load 1) array, affine
+vol_nii = nib.load('/content/104_mask_Gallbladder_rot.nii')
+affine = vol_nii.affine
+vol = vol_nii.get_fdata()
+
+# save both
+vol_nii = nib.Nifti1Image(vol, affine)
+vol_nii.to_filename('new.nii.gz')
