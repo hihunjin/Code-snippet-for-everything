130af873fdcb396473b9ed98d878e846d1fa4ea6
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-07-08 03:23:04 +0900

    Update load_save_nifty_nib.py

--- load_save_nifty_nib.py
+++ load_save_nifty_nib.py
@@ -4,7 +4,7 @@ import nibabel as nib
 # load 1) array, affine
 vol_nii = nib.load('/content/104_mask_Gallbladder_rot.nii')
 affine = vol_nii.affine
-vol = vol_nii.get_fdata()
+vol = vol_nii.get_fdata()       # default : np.float64
 
 # save both
 vol_nii = nib.Nifti1Image(vol, affine)
