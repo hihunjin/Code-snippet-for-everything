60acbd9ca6737db46886d8a2de09e817c3123c76
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-07-09 11:03:28 +0900

    Create pandas_save_parquet.py

---
+++ pandas/pandas_save_parquet.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+
+# make a dataframe
+df = pd.DataFrame(columns=['hi','im','david','.'])
+df_len = len(df)
+df.loc[df_len] = [3,1,2,0]
+
+
+# All column names must be 'string' types
+# df.columns = df.columns.astype(str)
+
+
+
+# save
+df.to_parquet('myfile.parquet', index=False)
+
+
+
+# print
+print(df)
+#   hi im david  .
+# 0  3  1     2  0
