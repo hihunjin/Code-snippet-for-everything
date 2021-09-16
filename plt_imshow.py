0d998eeee2811096f87f938ffe22386be639e5b1
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-09-17 01:28:01 +0900

    Update plt_imshow.py

--- plt_imshow.py
+++ plt_imshow.py
@@ -1,9 +1,10 @@
 # !wget https://thispersondoesnotexist.com/image -O 'image.png'
 
 import matplotlib.pyplot as plt
+import cv2
 
 # load
-image = cv2.imread("image.jpg")
+image = cv2.imread("image.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # show
