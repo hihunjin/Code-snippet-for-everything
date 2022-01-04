90cafe2eb204f35d78abbca1677e9453f8b3293c
hihunjin <32363064+hihunjin@users.noreply.github.com>
2022-01-04 15:41:49 +0900

    Update numpy_array_to_pillow.py

--- numpy_array_to_pillow.py
+++ numpy_array_to_pillow.py
@@ -2,9 +2,9 @@ from PIL import Image
 import numpy as np
 
 # 3d
-e = np.random.rand(150,150,3).astype('uint8')
+e = (np.random.rand(150,150,3) * 255).astype('uint8')
 out = Image.fromarray(e)                      #no error
 
 # 1d
-e = (np.random.rand(150,150) * 256).astype('uint8')
+e = (np.random.rand(150,150) * 255).astype('uint8')
 out = Image.fromarray(e, mode='L')            #no error
