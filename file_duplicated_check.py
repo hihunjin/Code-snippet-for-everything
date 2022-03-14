38a8f665d31d2fe9cf72b8e3d2c36c77094070e9
hihunjin <32363064+hihunjin@users.noreply.github.com>
2022-03-14 13:57:29 +0900

    Update file_duplicated_check.py

--- file_duplicated_check.py
+++ file_duplicated_check.py
@@ -2,15 +2,16 @@ import os
 
 dir = '/content'
 
-all_file_names = {}
-for path, subdirs, files in os.walk(dir):
-    for name in files:
-        if name in all_file_names:
-            all_file_names[name].append(os.path.join(path, name))
-        else:
-            all_file_names[name] = [os.path.join(path, name)]
-for keys, values in all_file_names.items():
-    if len(values) > 1:
-        print(f"The name '{keys}' is duplicated. loc : {values}")
-
+def file_duplicated_check(dir):
+    all_file_names = {}
+    for path, subdirs, files in os.walk(dir):
+        for name in files:
+            if name in all_file_names:
+                all_file_names[name].append(os.path.join(path, name))
+                print(f"the name '{name}' is duplicated. loc : {all_file_names[name]}")
+            else:
+                all_file_names[name] = [os.path.join(path, name)]
+    for keys, values in all_file_names.items():
+        if len(values) > 1:
+            print(f"The name '{keys}' is duplicated. loc : {values}")
 # The name 'hi' is duplicated. loc : ['/content/hi', '/content/sample_data/hi']
