fb3411e5c5cf2c14d85ca78b0917e95a1feddee6
hihunjin <32363064+hihunjin@users.noreply.github.com>
2023-03-26 13:16:07 +0900

    Update torchaudio_load.py

--- audio/torchaudio_load.py
+++ audio/torchaudio_load.py
@@ -1,4 +1,4 @@
-# !wget https://file-examples-com.github.io/uploads/2017/11/file_example_WAV_1MG.wav -O ex.wav
+# !wget https://file-examples.com/storage/feb401d325641db2fa1dfe7/2017/11/file_example_WAV_1MG.wav -O ex.wav
 # !wget http://www0.cs.ucl.ac.uk/teaching/GZ05/samples/tone.wav -O mono.wav
 # !pip install torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
