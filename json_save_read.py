9ce4a40eaf3820469a76ccd26b9c96287fbafd53
hihunjin <32363064+hihunjin@users.noreply.github.com>
2023-06-29 08:42:51 +0900

    Update json_save_read.py

--- json_save_read.py
+++ json_save_read.py
@@ -20,17 +20,21 @@ data['people'].append({
 
 
 ####### json save = dump #######
-with open('data.txt', 'w') as outfile:
-    json.dump(data, outfile)
-
-    
-
+import json
+def save_json(p, data):
+    with open(p, 'w') as outfile:
+        json.dump(data, outfile)
 ####### json read = load #######
 import json
+def load_json(p):
+    with open(p) as json_file:
+        data = json.load(json_file)
+        return data
 
-with open('data.txt') as json_file:
-    data = json.load(json_file)
-    for p in data['people']:
-        print('Name: ' + p['name'])
-        print('Website: ' + p['website'])
-        print('From: ' + p['from'])
+
+save_json("data.txt", data)
+data = load_json("data.txt")
+for p in data['people']:
+    print('Name: ' + p['name'])
+    print('Website: ' + p['website'])
+    print('From: ' + p['from'])
