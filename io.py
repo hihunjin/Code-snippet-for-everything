0b65737ab6be2e2f89262a40c41712160bd2a67e
hihunjin <32363064+hihunjin@users.noreply.github.com>
2023-08-12 15:17:44 +0900

    Create io.py

---
+++ io.py
@@ -0,0 +1,34 @@
+import json
+import pickle
+import numpy as np
+from typing import Any
+
+
+def pickle_dump(path: str, data: Any) -> None:
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
+
+
+def pickle_load(path: str) -> Any:
+    with open(path, "rb") as f:
+        return pickle.load(f)
+
+
+def npy_save(path: str, data: np.ndarray) -> None:
+    with open(path, "wb") as f:
+        np.save(f, data)
+
+
+def npy_load(path: str) -> np.ndarray:
+    with open(path, "rb") as f:
+        return np.load(f)
+
+
+def save_json(p, data) -> None:
+    with open(p, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+
+def load_json(p) -> Any:
+    with open(p) as json_file:
+        return json.load(json_file)
